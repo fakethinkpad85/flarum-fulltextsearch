9cb641f8788c798b666adc8f8bfb14e68cb89f84
Hüseyin Filiz <37220000+huseyinfiliz@users.noreply.github.com>
2025-03-01 08:25:33 +0300

    Update extend.php

--- extend.php
+++ extend.php
@@ -4,7 +4,7 @@ namespace fakethinkpad85\LikeSearch;
 
 use Flarum\Extend;
 use Flarum\Discussion\Search\Gambit\FulltextGambit;
-use fakethinkpad85\Search\Gambit\
+use fakethinkpad85\Search\Gambit\FulltextGambit;
 
 return [
     (new Extend\AddGambit(FulltextGambit::class))
